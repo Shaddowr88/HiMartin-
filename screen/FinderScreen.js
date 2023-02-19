b4251ea95df4d5a2b6851a9fb19423cea21d969a
Loic SUNVÉ <loicsunve@mini-de-loic.home>
2023-02-19 18:30:14 +0100

    first commit

---
+++ screen/FinderScreen.js
@@ -0,0 +1,12 @@
+import {SafeAreaView, Text} from "react-native";
+
+function ConcenteurScreen() {
+    return (
+        <SafeAreaView>
+<Text> Concenteur</Text>
+        </SafeAreaView>
+    );
+}
+
+
+export default ConcenteurScreen;
